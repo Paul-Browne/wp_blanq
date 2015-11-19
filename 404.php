62cb089a669b9ab586fd5c3b35244e40b91daa99
paulbrowne <Paulmatthewbrowne@gmail.com>
2015-11-19 14:19:37 +0200

    removed even more!!

--- 404.php
+++ 404.php
@@ -1,38 +1,7 @@
 <?php get_header(); ?>
-
-<?php /*
-<div id="content">
-    <div id="inner-content">
-        <main id="main" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
-            <article id="post-not-found" class="hentry">
-                <header class="article-header">
-                    <h1>
-                        <?php _e( 'Epic 404 - Article Not Found', 'bonestheme' ); ?>
-                    </h1>
-                </header>
-                <section class="entry-content">
-                    <p>
-                        <?php _e( 'The article you were looking for was not found, but maybe try looking again!', 'bonestheme' ); ?>
-                    </p>
-                </section>
-                <section class="search">
-                    <p>
-                    */ ?>
-                    
-                        <?php get_search_form(); ?>
-                    
-                    <?php /*
-                    </p>
-                </section>
-                <footer class="article-footer">
-                    <p>
-                        <?php _e( 'This is the 404.php template.', 'bonestheme' ); ?>
-                    </p>
-                </footer>
-            </article>
-        </main>
+<div class="fullscreen">
+    <div class="middlize">
+        <h1>Page not found</h1>
     </div>
 </div>
-*/ ?>
-
 <?php get_footer(); ?>
--- post-formats/format-audio.php
+++ post-formats/format-audio.php
@@ -1,24 +1,3 @@
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> role="article" itemscope itemtype="http://schema.org/AudioObject">
-<header class="article-header">
-<h1 class="entry-title single-title" itemprop="name"><?php the_title(); ?></h1>
-<p class="byline entry-meta vcard">
-<?php printf( __( 'Posted', 'bonestheme' ).' %1$s %2$s',
-'<time class="updated entry-time" datetime="' . get_the_time('Y-m-d') . '" itemprop="datePublished">' . get_the_time(get_option('date_format')) . '</time>',
-'<span class="by">'.__( 'by', 'bonestheme' ).'</span> <span class="entry-author author" itemprop="author" itemscope itemptype="http://schema.org/Person">' . get_the_author_link( get_the_author_meta( 'ID' ) ) . '</span>'
-); ?>
-</p>
-</header>
-<section class="entry-content" itemprop="description">
-<?php
-the_content();
-wp_link_pages( array(
-'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'bonestheme' ) . '</span>',
-'after'       => '</div>',
-'link_before' => '<span>',
-'link_after'  => '</span>',
-) ); ?>
-</section>
-<footer class="article-footer">
-<?php the_tags( '<p class="tags"><span class="tags-title">' . __( 'Tags:', 'bonestheme' ) . '</span> ', ', ', '</p>' ); ?>
-</footer>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> >
+<?php the_content(); ?>
 </article>
